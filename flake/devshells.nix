33da78310bd4eadcaccd6cf5f69d8494183f6f8d
Divit Mittal <64.69.76.69.74.m@gmail.com>
2026-04-05 11:08:44 +0530

    feat(flake): add devshell with python3, nixd and alejandra

---
+++ flake/devshells.nix
@@ -0,0 +1,36 @@
+{
+  inputs,
+  lib,
+  ...
+}: {
+  imports = [inputs.devshell.flakeModule];
+
+  perSystem = {
+    pkgs,
+    config,
+    ...
+  }: {
+    devshells.default = {
+      devshell = rec {
+        name = "awesome";
+        motd = "{202}Welcome to {91}${name} {202}devshell!{reset} \n $(menu)";
+        startup = {
+          git-hooks.text = ''
+            ${config.pre-commit.installationScript}
+          '';
+        };
+        packages = lib.attrsets.attrValues {
+          inherit
+            (pkgs)
+            ### LSPs & Formatters
+            ## Nix
+            nixd
+            alejandra
+            ### Star generator runtime
+            python3
+            ;
+        };
+      };
+    };
+  };
+}
