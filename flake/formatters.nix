0bd10185dcdb53a32abc67dc2e7772841ed17d9f
Divit Mittal <64.69.76.69.74.m@gmail.com>
2026-04-11 20:52:17 +0530

    feat(flake): add treefmt formatter with mdformat and alejandra

---
+++ flake/formatters.nix
@@ -0,0 +1,25 @@
+{inputs, ...}: {
+  imports = [inputs.treefmt-nix.flakeModule];
+
+  perSystem.treefmt = {
+    projectRootFile = "flake.nix";
+    settings.global = {
+      excludes = [
+        ".github/*"
+        "README.md"
+        "stars.md"
+      ];
+    };
+
+    flakeCheck = false;
+
+    programs = {
+      ## Nix
+      alejandra.enable = true;
+      deadnix.enable = true;
+      statix.enable = true;
+      ## Markdown
+      mdformat.enable = true;
+    };
+  };
+}
